b6f2ca472937db90a050378d9788da13c0c30d04
Ran Ge <range@tubi.tv>
2023-08-18 15:32:58 +0800

    change some code

--- lib/friends/test_person.ex
+++ lib/friends/test_person.ex
@@ -68,6 +68,24 @@ defmodule Friends.Test_Person do
     end)
   end
 
+  def test_fail_error_auto_rollback_catch_with_proper_ans_plain do
+    Friends.Repo.transaction(fn ->
+      john_update =
+        from Friends.Person,
+          where: [first_name: "John"],
+          update: [set: [first_name: "Ran"]]
+
+      try do
+        IO.puts("leading exception because uniq key constraint")
+        Friends.Repo.update_all(john_update, [])
+        IO.puts("can't not reach here because exception raised in the above line")
+      rescue
+        Postgrex.Error ->
+          IO.puts("Caught a Postgrex error")
+      end
+    end)
+  end
+
   def test_fail_manually_rollback_plain do
     Friends.Repo.transaction(fn ->
       john_update =
@@ -113,6 +131,74 @@ defmodule Friends.Test_Person do
           where: [first_name: "Ryan"],
           update: [inc: [age: 10]]
       {1, _} = Friends.Repo.update_all(ryan_update, [])
+    end)
+  end
+
+  def test_fail_error_catch_nested_tr_plain do
+    # without an outer transaction, return will be {:error, :rollback}
+    ans = test_fail_error_auto_rollback_catch_with_proper_ans_plain()
+    IO.inspect(ans)
+
+    # this change would rollback, because the exception below
+    Friends.Repo.transaction(fn ->
+      ryan_update =
+        from Friends.Person,
+          where: [first_name: "Ryan"],
+          update: [inc: [age: 10]]
+
+      {1, _} = Friends.Repo.update_all(ryan_update, [])
+      # within an outer transaction, the result will be {:ok, :ok}, this is so tricky
+      # TODO, need to investigate further
+      case ans = test_fail_error_auto_rollback_catch_with_proper_ans_plain() do
+        {:ok, _} ->
+          IO.inspect(ans)
+          IO.puts("success")
+        {:error, _} ->
+          IO.inspect(ans)
+          IO.puts("fail")
+        {_, _} -> IO.puts("shoud not reach here")
+      end
+
+      # code below can not run, because the exception above
+      ryan_update =
+        from Friends.Person,
+          where: [first_name: "Ryan"],
+          update: [inc: [age: 10]]
+      {1, _} = Friends.Repo.update_all(ryan_update, [])
+    end)
+  end
+
+  def test_fail_manual_rollback_nested_tr_plain do
+    # this change would rollback, because rolled back in the nested transaction
+    Friends.Repo.transaction(fn ->
+      ryan_update =
+        from Friends.Person,
+          where: [first_name: "Ryan"],
+          update: [inc: [age: 10]]
+      {1, _} = Friends.Repo.update_all(ryan_update, [])
+
+      case ans = test_fail_manually_rollback_plain() do
+        {:ok, _} ->
+          IO.inspect(ans)
+          IO.puts("success")
+        {:error, _} ->
+          IO.inspect(ans)
+          IO.puts("fail")
+        {_, _} -> IO.puts("shoud not reach here")
+      end
+
+      # code below would raise exception, because the transacation has already been rolled back
+      # ryan_update =
+      #   from Friends.Person,
+      #     where: [first_name: "Ryan"],
+      #     update: [inc: [age: 10]]
+      # {1, _} = Friends.Repo.update_all(ryan_update, [])
+
+      # or some other things
+      IO.puts("print something nonsense")
+
+      # can do manual rollback again
+      Friends.Repo.rollback("this is a random message")
     end)
   end
 end
